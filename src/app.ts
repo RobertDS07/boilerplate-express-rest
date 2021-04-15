a801386edeb80686cb6ecd07eaf6aba747efd69b
Robert Damaceno <robertdamacenobob@gmail.com>
2021-04-14 22:20:20 -0300

    fix: removing method start

--- src/app.ts
+++ src/app.ts
@@ -21,19 +21,13 @@ class App {
     }
 
     private middlewares() {
-        this.express.use(express.json)
-        this.express.use(cors)
+        this.express.use(express.json())
+        this.express.use(cors())
     }
 
     private routes() {
         this.express.use(routes)
     }
-
-    start() {
-        this.express.listen(process.env.PORT || 8081, () =>
-            console.log('listening on: http://localhost:8081'),
-        )
-    }
 }
 
-export default new App()
+export default new App().express
