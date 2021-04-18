9afc7313a25ba4a45489b3eaf6f88299cbb2d3e5
robertds07 <robertdamacenobob@gmail.com>
2021-04-18 05:11:58 -0300

    feat: base class of middlewares

---
+++ src/middlewares/baseClassMiddlewares.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express'
+
+class BaseClassMiddlewares {
+    all(req: Request, res: Response): void {
+        return
+    }
+
+    get(req: Request, res: Response, next: NextFunction): void {
+        this.all(req, res)
+        next()
+    }
+
+    find(req: Request, res: Response, next: NextFunction): void {
+        this.all(req, res)
+        next()
+    }
+
+    post(req: Request, res: Response, next: NextFunction): void {
+        this.all(req, res)
+        next()
+    }
+
+    patch(req: Request, res: Response, next: NextFunction): void {
+        this.all(req, res)
+        next()
+    }
+
+    delete(req: Request, res: Response, next: NextFunction): void {
+        this.all(req, res)
+        next()
+    }
+}
+
+export default BaseClassMiddlewares
