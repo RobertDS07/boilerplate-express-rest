fa48e137e1dd934fc8761b6561d1730f24852411
Robert Damaceno <robertdamacenobob@gmail.com>
2021-05-03 22:24:59 -0300

    fix: removing express-validator

--- src/middlewares/public/accountMiddlewares.ts
+++ src/middlewares/public/accountMiddlewares.ts
@@ -1,20 +1,5 @@
-import { NextFunction, Request, Response } from 'express'
-import { body, validationResult } from 'express-validator'
-
 import BaseClassMiddlewares from '../baseClassMiddlewares'
 
-class AccountMiddlewares extends BaseClassMiddlewares {
-    async post(req: Request, res: Response, next: NextFunction) {
-        await body('email', 'Invalid email').isEmail()(req, res, next)
-
-        const errors = validationResult(req)
-
-        if (!errors.isEmpty()) {
-            res.status(400).send({ errors: errors.array() })
-        }
-
-        next()
-    }
-}
+class AccountMiddlewares extends BaseClassMiddlewares {}
 
 export default new AccountMiddlewares()
