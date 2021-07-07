30ad39305e7a28d9e6f5751934d812759d84cfea
Robert Damaceno <robertdamacenobob@gmail.com>
2021-07-07 19:14:39 -0300

    fix(database settings): changing path to .js to use env

---
+++ configs/database.js
@@ -0,0 +1,36 @@
+module.exports = {
+    development: {
+        username: 'dev',
+        password: 'dev',
+        database: 'twitter',
+        host: 'localhost',
+        dialect: 'postgres',
+        port: 5432,
+        define: {
+            timestamps: true,
+            underscored: true,
+            deletedAt: true,
+            freezeTableName: true,
+            paranoid: true,
+        },
+    },
+    test: {
+        dialect: 'sqlite',
+        storage: '__tests__/database.sqlite',
+    },
+    production: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: 5432,
+        define: {
+            timestamps: true,
+            underscored: true,
+            deletedAt: true,
+            freezeTableName: true,
+            paranoid: true,
+        },
+    },
+}
