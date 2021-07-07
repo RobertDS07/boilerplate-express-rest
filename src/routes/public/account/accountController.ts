1aa898931559ebe71944ade28558de77a27887f3
Robert Damaceno <robertdamacenobob@gmail.com>
2021-07-07 19:19:42 -0300

    loading...

--- src/routes/public/account/accountController.ts
+++ src/routes/public/account/accountController.ts
@@ -2,9 +2,24 @@ import { Request, Response } from 'express'
 
 import BaseController from '../../baseController'
 
+import accountService from 'services/accountService'
+import tokenService from 'services/tokenService'
+
 class AccountController extends BaseController {
     async post(req: Request, res: Response): Promise<void> {
-        res.json(req.headers.oi).send()
+        try {
+            const { email, password, username } = req.body
+
+            const user = await accountService.createAccount({
+                email,
+                password,
+                username,
+            })
+
+            const token = tokenService.createToken(user)
+        } catch (e) {
+            console.log(e)
+        }
     }
 }
 
