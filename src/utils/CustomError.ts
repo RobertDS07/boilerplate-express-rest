558b8f171423856a355a02d819cfa3f755495467
Robert Damaceno <robertdamacenobob@gmail.com>
2021-07-16 21:02:08 -0300

    feat(CustomError.ts): adding method 'get body' to standardize body of error

---
+++ src/utils/CustomError.ts
@@ -0,0 +1,32 @@
+interface IError {
+    message: string
+}
+
+export interface IErrorBody {
+    errors: IError[]
+}
+
+class CustomError extends Error {
+    message: string
+    code?: number
+
+    constructor(message: string) {
+        super(message)
+
+        this.message = message
+    }
+
+    get body(): IErrorBody {
+        return {
+            errors: [{ message: this.message }],
+        }
+    }
+
+    businessException = (): this => {
+        this.code = 422
+
+        return this
+    }
+}
+
+export default CustomError
