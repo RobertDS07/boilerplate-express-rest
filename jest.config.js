4488821414d59e4c00a742db46bc0577f07663ba
Robert Damaceno <robertdamacenobob@gmail.com>
2021-07-17 21:05:40 -0300

    feat(jest.config.js): config to jest use paths of tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { compilerOptions } = require('./tsconfig.json')
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     modulePathIgnorePatterns: ['<rootDir>/dist/'],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: '<rootDir>',
+    }),
 }
