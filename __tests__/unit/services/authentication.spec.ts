abad9b6163b4e0e3c4959f7fef2906a96ef91ba0
Robert Damaceno <robertdamacenobob@gmail.com>
2021-07-23 23:02:16 -0300

    feat(authentication.spec.ts): tests to authenticationService

---
+++ __tests__/unit/services/authentication.spec.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import sequelize from '../../../src/sequelize'
+
+import AuthenticationService from '../../../src/services/authenticationService'
+
+import createUser from '../../utils/createUser'
+
+describe(`authenticationService tests`, () => {
+    beforeAll(async () => {
+        await sequelize.sync({ force: true })
+    })
+
+    it(`Should return user for correct login`, async () => {
+        const { rawData } = await createUser()
+
+        const dataToLogin = {
+            email: rawData.email,
+            password: rawData.password,
+        }
+
+        const user = await AuthenticationService.verifyAndGetUser(dataToLogin)
+
+        expect(user).toHaveProperty(`id`)
+    })
+
+    it(`Shouldnt return error for invalid login`, async () => {
+        try {
+            const dataToLogin = {
+                email: `a`,
+                password: `a`,
+            }
+
+            const user = await AuthenticationService.verifyAndGetUser(
+                dataToLogin,
+            )
+
+            //Shouldn't pass here
+
+            return false
+        } catch (e) {
+            const messageQuoteInvalid = e.message
+                .toLowerCase()
+                .includes(`invalid`)
+
+            expect(messageQuoteInvalid).toBeTruthy()
+        }
+    })
+})
