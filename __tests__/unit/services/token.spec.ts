6272a53491d9db3cbb344ede1777c5122bdf38ca
Robert Damaceno <robertdamacenobob@gmail.com>
2021-07-23 23:02:27 -0300

    feat(token.spec.ts): tests to tokenService

---
+++ __tests__/unit/services/token.spec.ts
@@ -0,0 +1,24 @@
+import sequelize from '../../../src/sequelize'
+
+import TokenService from '../../../src/services/tokenService'
+
+import createUser from '../../utils/createUser'
+
+describe(`tokenService tests`, () => {
+    beforeAll(async () => {
+        await sequelize.sync({ force: true })
+    })
+
+    it(`Should return a token and it must hasn't password of user`, async () => {
+        const { user } = await createUser(true)
+
+        const token = TokenService.createToken(user)
+
+        const decodedToken = TokenService.decodeToken(token)
+
+        const tokenIsString = typeof token === `string`
+        const tokenHasntPassword = !decodedToken.password
+
+        expect(tokenIsString && tokenHasntPassword).toBeTruthy()
+    })
+})
