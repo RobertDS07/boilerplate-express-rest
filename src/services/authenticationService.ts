ece8149f98c9db94fcd86adbe0dc455d96e43c83
Robert Damaceno <robertdamacenobob@gmail.com>
2021-07-23 23:00:25 -0300

    feat(authenticationService.ts): service create to verify if email and password is valid

---
+++ src/services/authenticationService.ts
@@ -0,0 +1,27 @@
+import bcrypt from 'bcryptjs'
+
+import { IUser } from 'models/Users'
+
+import UsersRepository from 'repositories/UsersRepository'
+
+import CustomError from 'utils/CustomError'
+
+type TGetUser = Required<Pick<IUser, `email` | `password`>>
+
+class AuthenticationService {
+    async verifyAndGetUser({ email, password }: TGetUser): Promise<IUser> {
+        const user = await UsersRepository.getByEmail(email)
+
+        const invalidPassword =
+            user && !bcrypt.compare(password, user?.password)
+
+        if (!user || invalidPassword)
+            throw new CustomError(`Invalid email or password`).AccessDenied()
+
+        const userWithoutPassword = { ...user, password: undefined }
+
+        return userWithoutPassword
+    }
+}
+
+export default new AuthenticationService()
