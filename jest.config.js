d65b908800dacf6ad3d700d3baf7b9ebbc90c413
Robert Damaceno <robertdamacenobob@gmail.com>
2021-07-23 22:54:45 -0300

    feat(.eslintrc): quotes = backtick

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
-const { compilerOptions } = require('./tsconfig.json')
+const { pathsToModuleNameMapper } = require(`ts-jest/utils`)
+const { compilerOptions } = require(`./tsconfig.json`)
 
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    modulePathIgnorePatterns: ['<rootDir>/dist/'],
+    preset: `ts-jest`,
+    testEnvironment: `node`,
+    modulePathIgnorePatterns: [`<rootDir>/dist/`, `__tests__/utils/`],
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-        prefix: '<rootDir>',
+        prefix: `<rootDir>`,
     }),
 }
